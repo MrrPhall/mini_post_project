ceeaf4ffa2b36306b3a470f6eb7caa4ff7e16a4c
E-Phall <gmsuser@Macbooks-MacBook-Pro.local>
2024-08-19 18:08:17 +0700

    initialize project

---
+++ src/main/java/com/phall/mini_post_project/MiniPostProjectApplication.java
@@ -0,0 +1,13 @@
+package com.phall.mini_post_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPostProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPostProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phall/mini_post_project/MiniPostProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phall.mini_post_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPostProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
